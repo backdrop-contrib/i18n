8f56a807718e24ccddeef9373cede9134ed622a5
indigoxela <user@olga.indigofloat.at>
2022-01-19 13:48:03 +0100

    That did not work

--- .github/misc/settings.local.php
+++ .github/misc/settings.local.php
@@ -4,8 +4,5 @@
  * Custom settings for test runs.
  */
 
-// Database settings.
-$database = 'mysql://root:root@127.0.0.1/backdrop';
-
 // Never send telemetry data.
 $settings['telemetry_enabled'] = FALSE;
