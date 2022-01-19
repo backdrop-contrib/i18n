507bd859357a3d1878274e3c22a08ffcf632c2ca
indigoxela <user@olga.indigofloat.at>
2022-01-19 13:39:43 +0100

    Restructure GHA, prevent telemetry data

---
+++ .github/misc/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Custom settings for test runs.
+ */
+
+// Database settings.
+$database = 'mysql://root:root@127.0.0.1/backdrop';
+
+// Never send telemetry data.
+$settings['telemetry_enabled'] = FALSE;
